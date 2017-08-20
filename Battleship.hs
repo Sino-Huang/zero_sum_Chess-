734b016cf65ba36da3d25d407cb38b7170da1dbc
SUKAI at home <huangsukai1997@gmail.com>
2017-08-20 23:32:28 +1000

    not finished work

--- Battleship.hs
+++ Battleship.hs
@@ -6,10 +6,10 @@
 -- without the explicit permission from the original author. 
 --
 -- Assignment completed by:
--- Name    :
--- UID     :
--- Tutor   :
--- Lab Time: 
+-- Name    :Sukai Huang
+-- UID     :u6492211
+-- Tutor   :Debashish Chakraborty @u4610248
+-- Lab Time:Thursday 3pm
 
 module Battleship where
 
@@ -29,8 +29,14 @@ showBoard cells = "+" ++ replicate 21 '-' ++ "+\n"
         rowsToGrid row = "| " ++ intercalate " " (map cStateToCell row) ++ " |"
         
         cStateToCell :: Cell -> String
-        cStateToCell = undefined
-    
+        cStateToCell cell = case cell of
+            Unchecked -> " "
+            Hit -> "o"
+            Miss -> "x"
+
+
+
+
 data Cell = Unchecked | Hit | Miss
     deriving (Show, Read)
 type Matrix a  = [Row a]
@@ -75,7 +81,12 @@ validPlacement gs c d s =
         onCoords = getCoords c d (shipLength s)
         
 shipLength :: ShipType -> Integer
-shipLength = undefined
+shipLength shipl = case shipl of
+    Carrier -> 5
+    Battleship -> 4
+    Cruiser -> 3
+    Submarine -> 3
+    Destroyer -> 2
     
 -- getCoords returns the list of Coordinate which would be
 -- occupied by the ship.
@@ -92,7 +103,9 @@ getNeighbours :: Coordinate -> [Coordinate]
 getNeighbours (x, y) = [(i, j) | i <- [(x - 1) .. (x + 1)], j <- [(y - 1) .. (y + 1)]]
     
 coordInBound :: Coordinate -> Bool
-coordInBound = undefined
+coordInBound (x, y)
+    | x >= 0 && x <= 9 && y>= 0 && y <= 9 = True
+    | otherwise = False
 
 -- isShipAtCoord determines if there is a ship already placed
 -- at the coord.
@@ -100,7 +113,77 @@ isShipAtCoord :: Coordinate -> Ships -> Bool
 isShipAtCoord (x, y) grid = grid !! (fromIntegral y) !! (fromIntegral x)
 
 placeShip :: GenShips -> Coordinate -> Direction -> ShipType -> GenShips
-placeShip = undefined
+placeShip gen (x,y) direc shipt =  gen{gsShips = case direc of
+                                          Down -> case shipt of
+                                              Carrier ->  updatesimpledown 5 (gsShips gen) x y
+                                              Battleship ->  updatesimpledown 4 (gsShips gen) x y
+                                              Cruiser ->  updatesimpledown 3 (gsShips gen) x y
+                                              Submarine ->  updatesimpledown 3 (gsShips gen) x y
+                                              Destroyer ->  updatesimpledown 2 (gsShips gen)x y
+                                          Right ->case shipt of
+                                              Carrier ->  updatesimpleright 5 (gsShips gen) x y
+                                              Battleship ->  updatesimpleright 4 (gsShips gen) x y
+                                              Cruiser ->  updatesimpleright 3 (gsShips gen) x y
+                                              Submarine ->  updatesimpleright 3 (gsShips gen) x y
+                                              Destroyer ->  updatesimpleright 2 (gsShips gen) x y
+
+                                          Up ->case shipt of
+                                              Carrier ->  updatesimpleup 5 (gsShips gen) x y
+                                              Battleship ->  updatesimpleup 4 (gsShips gen) x y
+                                              Cruiser ->  updatesimpleup 3 (gsShips gen) x y
+                                              Submarine ->  updatesimpleup 3 (gsShips gen) x y
+                                              Destroyer ->  updatesimpleup 2 (gsShips gen) x y
+
+                                          Left ->case shipt of
+                                              Carrier ->  updatesimpleleft 5 (gsShips gen) x y
+                                              Battleship ->  updatesimpleleft 4 (gsShips gen) x y
+                                              Cruiser ->  updatesimpleleft 3 (gsShips gen) x y
+                                              Submarine ->  updatesimpleleft 3 (gsShips gen) x y
+                                              Destroyer ->  updatesimpleleft 2 (gsShips gen) x y ,
+
+                                      existingShips = shipt : (existingShips (gen)),
+                                      finished = case length(existingShips (gen)) of
+                                          5 -> True
+                                          _ -> False
+}
+
+    where
+
+    updatesimpledown :: Integer -> Ships -> Integer -> Integer  -> Ships
+    updatesimpledown length ships coordx coordy
+        | length == 1 = updateList ships (fromIntegral coordy) (updateList (ships !! (fromIntegral coordy)) (fromIntegral coordx) True)
+        | length > 1  =  updatesimpledown 1 (updatesimpledown (length - 1) ships coordx coordy) coordx (coordy + length - 1)
+    updatesimpleup :: Integer -> Ships -> Integer -> Integer  -> Ships
+    updatesimpleup length ships coordx coordy
+        | length == 1 = updateList ships (fromIntegral coordy) (updateList (ships !! (fromIntegral coordy)) (fromIntegral coordx) True)
+        | length > 1  =  updatesimpledown 1 (updatesimpledown (length - 1) ships coordx coordy) coordx (coordy - length + 1)
+    updatesimpleleft :: Integer -> Ships -> Integer -> Integer  -> Ships
+    updatesimpleleft length ships coordx coordy
+        | length == 1 = updateList ships (fromIntegral coordy) (updateList (ships !! (fromIntegral coordy)) (fromIntegral coordx) True)
+        | length > 1  =  updatesimpledown 1 (updatesimpledown (length - 1) ships coordx coordy) (coordx-length + 1) coordy
+    updatesimpleright :: Integer -> Ships -> Integer -> Integer  -> Ships
+    updatesimpleright length ships coordx coordy
+        | length == 1 = updateList ships (fromIntegral coordy) (updateList (ships !! (fromIntegral coordy)) (fromIntegral coordx) True)
+        | length > 1  =  updatesimpledown 1 (updatesimpledown (length - 1) ships coordx coordy) (coordx+length-1) coordy
+
 
 transitionState :: State -> Coordinate -> State
-transitionState = undefined
+transitionState state coord = undefined
+
+testnewships :: Ships
+testnewships = replicate 10 (replicate 10 False)
+
+testnewgenship :: GenShips
+testnewgenship = GenShips {gsShips = testnewships , existingShips =[], finished =False}
+
+testnewboard :: Board
+testnewboard = replicate 10 (replicate 10 Unchecked)
+
+testnewstate :: State
+testnewstate = State {board = testnewboard, ships = testnewships, condition = Playing, numMoves = 0 }
+
+getShips :: GenShips -> Ships
+getShips GenShips{gsShips = n } = n
+
+getShipType :: GenShips -> [ShipType]
+getShipType GenShips {existingShips = n} = n
